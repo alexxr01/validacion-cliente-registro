8b0dc1f9fe35f6422c81d1dd261655b5ec711980
Alex <aruibon154@g.educaand.es>
2023-03-07 12:21:31 +0100

    Avances

--- app.js
+++ app.js
@@ -1,25 +1,25 @@
 const express = require('express')
 const app = express()
-const {body, validationResult} = require('express-validator')
+const { body, validationResult } = require('express-validator')
 app.use(express.json())
 app.set('view engine', 'ejs')
-app.use(express.urlencoded({extended:true}))
+app.use(express.urlencoded({ extended: true }))
 
-app.get('/', (req, res)=>{
+app.get('/', (req, res) => {
     res.render('index')
 })
 
 app.post('/registrar', [
-    body('usuario', 'El usuario debetener al menos 5 carácteres')
+    body('usuario', 'El usuario debe tener al menos 5 carácteres')
         .exists()
-        .isLength({min:5}),
+        .isLength({ min: 5 }),
     body('email', 'El email introducido no es válido.')
         .exists()
         .isEmail(),
-    body('contrasena', 'La contraseña debe tener al menos 5 carácteres')        
+    body('contrasena', 'La contraseña debe tener al menos 5 carácteres')
         .exists()
-        .isLength({min:5})
-], (req, res)=>{
+        .isLength({ min: 5 })
+], (req, res) => {
     //Validación de la documentación oficial
     /* const errors = validationResult(req);
     if (!errors.isEmpty()) {
@@ -33,11 +33,11 @@ app.post('/registrar', [
         console.log(req.body)
         const valores = req.body
         const validaciones = errors.array()
-        res.render('index', {validaciones:validaciones, valores: valores})
-    }else{
+        res.render('index', { validaciones: validaciones, valores: valores })
+    } else {
         res.send('¡Validación Exitosa!')
     }
 })
-app.listen(3000, ()=>{
+app.listen(3000, () => {
     console.log('El servidor se ha iniciado en http://localhost:3000')
 })
